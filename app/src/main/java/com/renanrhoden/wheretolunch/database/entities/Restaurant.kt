ea413cdb17becb9a1df6c116963f91e8b2aad159
Renan Rhoden <renanrhoden@gmail.com>
2019-03-02 18:49:35 -0300

    listing restaurants

--- app/src/main/java/com/renanrhoden/wheretolunch/database/entities/Restaurant.kt
+++ app/src/main/java/com/renanrhoden/wheretolunch/database/entities/Restaurant.kt
@@ -1,12 +1,41 @@
 package com.renanrhoden.wheretolunch.database.entities
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 @Entity(tableName = "restaurants")
 data class Restaurant(
     @PrimaryKey
     val name: String,
     val address: String,
     val photoReference: String
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(address)
+        parcel.writeString(photoReference)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Restaurant> {
+        override fun createFromParcel(parcel: Parcel): Restaurant {
+            return Restaurant(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Restaurant?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
